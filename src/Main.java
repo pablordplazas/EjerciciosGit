051c788dc9c2b0d18e11a0f7c822cdb66a2a6b38
pablordplazas <pablordplazas@gmail.com>
2025-08-22 17:22:58 -0500

    agregue codigo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("HOLA MUNDO");
+        System.out.println("Pablo Ortiz Plazas");
+        System.out.println("Triztemente desesperado");
 
     }
 }
