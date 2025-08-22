d1223e9fd9c172836135b8ac2e4509b481631650
pablordplazas <pablordplazas@gmail.com>
2025-08-22 17:19:40 -0500

    CAMBIOS

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("hola mundo");
-        System.out.println("Pablo ortiz Plazas");
-
+        System.out.println();
 
     }
 }
